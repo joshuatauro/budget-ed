aee071a3de58260570f0749afe5bbc2b96d843ae
joshuatauro <joshuatauro45@gmail.com>
2023-04-09 19:16:19 +0530

    initialised notes app

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from "react-native"
+import { useRouter, Stack } from "expo-router"
+const Home = () => {
+  return(
+
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  )
+}
